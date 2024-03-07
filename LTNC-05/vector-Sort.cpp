b9df123312bb3be65b7584a2f48bf7a126152370
mthang1201 <89679587+mthang1201@users.noreply.github.com>
2024-03-07 08:44:18 +0700

    Create vector-Sort.cpp

---
+++ LTNC-05/vector-Sort.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n; cin >> n;
+    vector<int> A(n);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    return 0;
+}
