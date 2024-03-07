334bc5d491ab1ea092598e6186c40d137c367bad
mthang1201 <89679587+mthang1201@users.noreply.github.com>
2024-03-07 08:17:35 +0700

    Create basicDataTypes.cpp

---
+++ LTNC-01/basicDataTypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    string str;
+    while (cin >> str) {
+        cout << str << endl;
+    }
+    return 0;
+}
