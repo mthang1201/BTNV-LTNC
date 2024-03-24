503972a769f12c327e204678141b6444a46ed04f
mthang1201 <89679587+mthang1201@users.noreply.github.com>
2024-03-24 11:00:54 +0700

    Create day26_nestedLogic.cpp

---
+++ LTNC-09/day26_nestedLogic.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int d1, m1, y1;
+    int d2, m2, y2;
+    
+    cin >> d1 >> m1 >> y1;
+    cin >> d2 >> m2 >> y2;   
+    
+    if (y1 > y2) cout << 10000;
+    else if (y1 < y2) cout << 0;
+    else if (m1 > m2 && y1 == y2) cout << (m1 - m2) * 500;
+    else if (m1 < m2 && y1 == y2) cout << 0;
+    else if (d1 > d2 && m1 == m2) cout << (d1 - d2) * 15;
+    else cout << 0;
+    
+    return 0;
+}
