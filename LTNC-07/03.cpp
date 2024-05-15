23ff537549306746ac05f2a6bd27b67aca4d5cda
mthang1201 <89679587+mthang1201@users.noreply.github.com>
2024-05-15 15:23:27 +0700

    Create 03.cpp

---
+++ LTNC-07/03.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'powerSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER X
+ *  2. INTEGER N
+ */
+int helper(int X, int N, int sum, int num) {
+    if (sum == X)
+        return 1;
+    if (sum > X || pow(num ,N) > X)
+        return 0;
+    return helper(X, N, sum, num + 1) + helper(X, N, sum + pow(num, N), num + 1);
+}
+
+int powerSum(int X, int N) {
+    int cnt = 0;
+    cnt += helper(X, N, 0, 1);
+    return cnt;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string X_temp;
+    getline(cin, X_temp);
+
+    int X = stoi(ltrim(rtrim(X_temp)));
+
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    int result = powerSum(X, N);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
